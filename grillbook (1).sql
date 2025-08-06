505d8aaef904ffc280c7f815e1f506cf8b47d704
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-06 23:52:22 +0800

    crud menu

--- grillbook (1).sql
+++ grillbook (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2025 at 06:01 PM
+-- Generation Time: Aug 06, 2025 at 05:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `grillbook`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL,
+  `menu_name` varchar(60) NOT NULL,
+  `menu_description` text DEFAULT NULL,
+  `menu_price` decimal(10,2) NOT NULL,
+  `menu_image_banner` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -42,12 +56,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_fname`, `user_lname`, `user_email`, `user_password`, `user_position`, `user_status`) VALUES
-(1, 'Joshua Anderson', 'Padilla', 'joshua@gmail.com', '$2y$10$YEbThVqGyDoR0EcowWqu/.Sghc0mSg3ufY7JsSnz9465RkJ8PMDhK', 'customer', 1),
-(2, 'juan', 'dela cruz', 'juan@gmail.com', '$2y$10$vVx7ajAOSm/sVH6CM2.JLeDxig7eWF7w0r2di8.86Ibjv.pHDBkaO', 'admin', 1);
+(2, 'juan', 'dela cruz', 'admin@gmail.com', '$2y$10$365uaOv8W44iLlOqJYyIgukb4WKci2BpbHEj9272Y0CdrQLd.0sdq', 'admin', 1),
+(3, 'Joshua Anderson', 'Padilla', 'andersonandy046@gmail.com', '$2y$10$wT2P4z2/HuwbW1Dcgb/zleHRj62t2f0XXPiiUrbB2s/xhJj8p0I.W', 'customer', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu_id`);
 
 --
 -- Indexes for table `user`
@@ -60,10 +80,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
