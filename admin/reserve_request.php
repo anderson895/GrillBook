fd1e60bec22676ba9c8122be78df67f750482a81
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-10 20:03:01 +0800

    fetch reservation request

--- admin/reserve_request.php
+++ admin/reserve_request.php
@@ -6,7 +6,7 @@ include "../src/components/admin/nav.php";
 
 <!-- Top Bar -->
 <div class="flex justify-between items-center bg-[#0D0D0D] p-4 mb-6 rounded-md shadow-lg">
-  <h2 class="text-xl font-bold text-[#FFD700] uppercase tracking-wide">Gettable</h2>
+  <h2 class="text-xl font-bold text-[#FFD700] uppercase tracking-wide">Reservation Request</h2>
   <div class="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center text-white font-bold shadow-md">
     <?php echo strtoupper(substr($_SESSION['user_fname'], 0, 1)); ?>
   </div>
